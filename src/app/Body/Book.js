a8ea3e7d144a4ffc83533a67fe5bf95a0da6962e
Aravind1505 <karavind246815@gmail.com>
2023-11-15 00:57:37 +0530

    Added Like icon(no functionality), output book edition on click

--- src/app/Body/Book.js
+++ src/app/Body/Book.js
@@ -1,13 +1,25 @@
+import { IoMdHeartEmpty } from "react-icons/io";
+
 export default function Book(props) {
+
+    function test(event) {
+        const __book = event.target.parentNode.parentNode;
+        const __edition = __book.childNodes[0].childNodes[0].getAttribute("src").split("/")[5].split("-")[0];
+        console.log(__edition);
+    }
+
     const cover = "https://covers.openlibrary.org/b/olid/" + props.cover + "-M.jpg";
     return <div className="Book flex flex-row gap-x-5 p-4 bg-blue-300 rounded-md">
         <div className="h-44">
-            <img src={cover} alt="image" className=" Image rounded-md max-h-44" >{props.image}</img>
+            <img src={cover} alt="image" className=" Image rounded-md max-h-44">{props.image}</img>
         </div>
-        <div className="flex flex-col gap-y-1">
-            <div className="Title">{props.title}</div>
-            <div className="Author text-sm">{props.author}</div>
-            <div className="Rating text-sm">{props.rating}</div>
+        <div className="flex flex-col justify-between">
+            <div className="flex flex-col gap-y-1">
+                <div className="Title">{props.title}</div>
+                <div className="Author text-sm">{props.author}</div>
+                <div className="Rating text-sm">{props.rating}</div>
+            </div>
+            <IoMdHeartEmpty className="text-3xl" onClick={test}/>
         </div>
     </div>
 }
--- src/app/Body/PageBody.js
+++ src/app/Body/PageBody.js
@@ -6,9 +6,7 @@ export default function PageBody(props) {
         <ul className="grid gap-x-8 gap-y-5 p-5 grid-cols-4">
             {props.books.map((e) => {
                 return (
-                    <div key={e.key}>
-                        <Book title={e.title} author={e.author_name} rating={e.ratings_average} isbn={e.isbn} cover={e.cover_edition_key}/>
-                    </div>
+                    <Book key={e.key} title={e.title} author={e.author_name} rating={e.ratings_average} isbn={e.isbn} cover={e.cover_edition_key} />
                 );
             })}
         </ul>
