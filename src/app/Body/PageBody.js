6daa0cd1a230872e11f15f5e4ed7d279a7345a7c
Aravind1505 <karavind246815@gmail.com>
2023-11-15 00:24:35 +0530

    Changed to 4 columns

--- src/app/Body/PageBody.js
+++ src/app/Body/PageBody.js
@@ -3,7 +3,7 @@ import Book from "./Book";
 export default function PageBody(props) {
 
     return <div className="flex w-full h-full text-black  ">
-        <ul className="grid gap-x-8 gap-y-5 p-5 grid-cols-5">
+        <ul className="grid gap-x-8 gap-y-5 p-5 grid-cols-4">
             {props.books.map((e) => {
                 return (
                     <div key={e.key}>
